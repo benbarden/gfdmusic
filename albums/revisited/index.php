5775de1e3d60965940167060cd2bda9b001974cf
Ben Barden <github@benbarden.com>
2016-09-25 13:51:11 +0100

    Correcting time of track 1

--- albums/revisited/index.php
+++ albums/revisited/index.php
@@ -72,7 +72,7 @@
     require '../../includes/core/scripts.php';
     require '../../includes/player/base.php';
 ?>
-<script src="/assets/js/albums/revisited.js?v=1"></script>
+<script src="/assets/js/albums/revisited.js?v=1a"></script>
 <?php
     require '../../includes/core/footer.php';
 ?>
--- assets/js/albums/revisited.js
+++ assets/js/albums/revisited.js
@@ -2,5 +2,5 @@ var bbMusicTracklist = [];
 bbMusicTracklist.push({
     title: "01. The Cliffs (Revisited Remix)",
     mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('01. The Cliffs (Revisited Remix).mp3'),
-    duration: "3:30"
+    duration: "3:14"
 });
