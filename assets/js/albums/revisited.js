ebc59dca46a9db63faeab47e07f9d979dde0b1e1
Ben Barden <github@benbarden.com>
2016-10-01 22:34:17 +0100

    Adding track 2

--- assets/js/albums/revisited.js
+++ assets/js/albums/revisited.js
@@ -4,3 +4,8 @@ bbMusicTracklist.push({
     mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('01. The Cliffs (Revisited Remix).mp3'),
     duration: "3:14"
 });
+bbMusicTracklist.push({
+    title: "02. Journey to Atlantis (Revisited Remix)",
+    mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('02. Journey to Atlantis (Revisited Remix).mp3'),
+    duration: "6:43"
+});
