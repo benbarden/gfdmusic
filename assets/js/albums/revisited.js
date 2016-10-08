d74b4126353779b671a679f51ec308cb258e3ec7
Ben Barden <github@benbarden.com>
2016-10-08 16:15:14 +0100

    Adding track 3

--- assets/js/albums/revisited.js
+++ assets/js/albums/revisited.js
@@ -9,3 +9,8 @@ bbMusicTracklist.push({
     mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('02. Journey to Atlantis (Revisited Remix).mp3'),
     duration: "6:43"
 });
+bbMusicTracklist.push({
+    title: "03. The Sword (Revisited Remix)",
+    mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('03. The Sword (Revisited Remix).mp3'),
+    duration: "4:26"
+});
