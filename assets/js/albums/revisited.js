5b68cf721423788e40314e5525d44bcbe75fd574
Ben Barden <github@benbarden.com>
2016-10-15 21:38:17 +0100

    Track 4

--- assets/js/albums/revisited.js
+++ assets/js/albums/revisited.js
@@ -14,3 +14,8 @@ bbMusicTracklist.push({
     mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('03. The Sword (Revisited Remix).mp3'),
     duration: "4:26"
 });
+bbMusicTracklist.push({
+    title: "04. Somewhere There (Revisited Remix)",
+    mp3: "http://coda.city/downloads/gfdmusic/revisited/" + encodeURI('04. Somewhere There (Revisited Remix).mp3'),
+    duration: "3.39"
+});
